5d58ef350baa91140e57ed6dd82419dd3865a648
hudutrip <126909852+hudutrip@users.noreply.github.com>
2024-06-30 23:08:48 +0000

    dcit208_project
    dcit208_project
    
    Co-Authored-By: Nelson Soh <89706212+nelsonsoh@users.noreply.github.com>

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+          <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1570174006382-148305ce4972?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="absolute inset-0 h-full
+              rounded-3xl
+               w-full object-cover"
+            />
+          </div>
+
+          <div className="lg:py-24">
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              <b>Find & Book Spa Appointment With Us at <span className='text-primary'>Golden View</span></b></h2>
+
+            <p className="mt-4 text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+              eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
+              quidem quam repellat.
+            </p>
+
+            <a
+              href="#"
+              className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              Get Started Today
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
